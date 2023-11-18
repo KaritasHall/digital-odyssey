e941c3338f64787dbc9088b1df07bea088ed5c23
Karitas <karitashall@gmail.com>
2023-11-18 13:02:32 +0000

    Modal pop up for reset and localStorge/db

--- src/app/components/reset-button/index.tsx
+++ src/app/components/reset-button/index.tsx
@@ -1,7 +1,20 @@
 // Function to handle session reset that returns a Start over button
+import { useState } from "react";
 import { TEMP_GAME_STATE_LOCALSTORAGE } from "../auth-button";
+import { Modal } from "../modal";
+
+const resetSessionOptions = { optionA: "Yes", optionB: "No" };
 
 export const ResetButton = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleResetConfirmation = (choice: string) => {
+    setIsModalOpen(false);
+    if (choice === resetSessionOptions.optionA) {
+      resetSession();
+    }
+  };
+
   const resetSession = async () => {
     try {
       // API call to delete the user's session from the database
@@ -16,11 +29,21 @@ export const ResetButton = () => {
   };
 
   return (
-    <button
-      className="text-sm text-player hover:text-storyteller"
-      onClick={resetSession}
-    >
-      Start Over
-    </button>
+    <>
+      <button
+        className="hover:text-storyteller text-player"
+        onClick={() => setIsModalOpen(true)}
+      >
+        Start Over
+      </button>
+
+      <Modal
+        onChoice={handleResetConfirmation}
+        title="Start Over"
+        description="Are you sure you want to start over? This will delete your current progress."
+        buttonLabel={resetSessionOptions}
+        isModalOpen={isModalOpen}
+      />
+    </>
   );
 };
